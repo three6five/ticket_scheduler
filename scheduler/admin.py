72e9398ed1686ff006855ec1fc79f958a6c2bdaf
ShaseOfBase <travis.weedon@three6five.com>
2022-07-15 16:03:24 +0200

    updates

--- scheduler/admin.py
+++ scheduler/admin.py
@@ -6,16 +6,11 @@ from django.contrib.admin import AdminSite
 admin.site.site_header = 'Ticket Scheduler'
 
 
-class TaskInline(admin.TabularInline):
-    model = Task
+class TaskAdmin(admin.ModelAdmin):
+    date_hierarchy = 'start_date'
 
     def get_readonly_fields(self, request, obj=None):
         return ["last_run_time"]
-
-
-class TaskAdmin(admin.ModelAdmin):
-    date_hierarchy = 'start_date'
-  #  inlines = TaskInline,
 
 
 admin.site.register(Task, TaskAdmin)
