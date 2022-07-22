f46453c98c401c8cc77ad298a4dfabc8e3eca50b
ShaseOfBase <travis.weedon@three6five.com>
2022-07-22 15:23:17 +0200

    updates

---
+++ ticket_scheduler_app/env.py
@@ -0,0 +1,14 @@
+#re_sql_host = '127.0.0.1'
+#re_sql_port = '3306'
+#re_sql_user = 'admin'
+#re_sql_pass = 'Alcapone86!'
+#re_sql_db = 'reporting_engine'
+
+#prod
+re_sql_host = '10.1.126.16'
+re_sql_port = '3306'
+re_sql_user = 'reporting_engine_user'
+re_sql_pass = 'AISHD89hdn12jkndansd'
+re_sql_db = 'reporting_engine'
+
+fd_api_key = 'KKWtaYkQAerzoXSwbqG'
