94bcc7856c246a133ac4a22f0fc0a26f4d37055b
ShaseOfBase <travis.weedon@three6five.com>
2022-07-22 15:28:33 +0200

    updates

--- ticket_scheduler_app/manage.py
+++ ticket_scheduler_app/manage.py
@@ -16,7 +16,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    if 'runserver' in sys.argv:
+    if 'runserver' in sys.argv or 'uwsgi' in sys.argv:
         settings.IS_RUNNING = True
 
     execute_from_command_line(sys.argv)
