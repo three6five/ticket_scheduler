79c0239fda57553d2492ee990d84d0218792fea2
ShaseOfBase <travis.weedon@three6five.com>
2022-07-29 16:43:11 +0200

    updates

---
+++ env.py
@@ -0,0 +1,13 @@
+DEV_MODE = 0
+
+re_sql_host_dev = '127.0.0.1'
+re_sql_pass_dev = 'Haskjd1jhkb2jkb123DJNwbj!'
+
+#prod
+re_sql_host = '10.1.126.16'
+re_sql_port = '3306'
+re_sql_user = 'reporting_engine_user'
+re_sql_pass = 'AISHD89hdn12jkndansd'
+re_sql_db = 'reporting_engine'
+
+fd_api_key = 'KKWtaYkQAerzoXSwbqG'
